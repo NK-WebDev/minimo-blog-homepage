f6bca7f357794570c096bf582008e1b4c7f776e3
Nirjan Khadka <webdev.nk@gmail.com>
2017-08-11 11:26:01 +0545

    completed the html,css and js
    - completed blog-post section
    - completed featured post section
    - completed newsletter section and footer
    - cleaned up the code

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,5 +1,7 @@
+//constants
 const NAVTOGGLE=document.querySelector(".nav-toggle");
 const NAVMENU=document.querySelector("nav");
+// navigation menu for smaller screens
 NAVTOGGLE.onclick=function () {
     this.classList.toggle('open');
     NAVMENU.classList.toggle('show');
